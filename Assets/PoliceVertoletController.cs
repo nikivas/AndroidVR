302c93b4b9ca42b26bc17935087766249042e316
nikivas <nikivas97@mail.ru>
2018-04-27 23:08:14 +0400

    'bigLos'

---
+++ Assets/PoliceVertoletController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PoliceVertoletController : MonoBehaviour {
+
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float x = CrossPlatformInputManager.GetAxis ("Horizontal");
+		float y = CrossPlatformInputManager.GetAxis ("Vertical");
+
+		Vector3 move = new Vector3 (x, 0, y);
+
+		rb.velocity = move * 4.0f;
+
+		if(x != 0 && y != 0){
+
+			transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Atan2(x,y) * Mathf.Rad2Deg ,transform.eulerAngles.z);
+		}
+
+	}
+}
---
+++ Assets/Vuforia/Scripts/VBDragonScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using UnityStandardAssets.CrossPlatformInput;
+public class VBDragonScript : MonoBehaviour, IVirtualButtonEventHandler {
+	private GameObject VButtonObject;
+	private GameObject dragon;
+	private Animation dragonAnimation;
+	// Use this for initialization
+	void Start () {
+		VButtonObject = GameObject.Find ("VirtualButtonDragon");
+		VButtonObject.GetComponent<VirtualButtonBehaviour> ().RegisterEventHandler(this);
+
+		dragon = GameObject.Find ("Mountain Dragon");
+		dragonAnimation = dragon.GetComponent<Animation> ();
+
+	}
+
+	public void OnButtonPressed(VirtualButtonAbstractBehaviour vb){
+		dragonAnimation.Play ("4leg3HitComboTailAttack");
+		Debug.Log ("Pressed");
+	}
+	public void OnButtonReleased(VirtualButtonAbstractBehaviour vb){
+//		dragonAnimation.Stop ();
+		Debug.Log ("UNPRESSED");
+	}
+}
